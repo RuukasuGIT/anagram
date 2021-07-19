77f79d7f9f55ef1b01198bfccebd392e101ee223
Ruukasu <63557605+RuukasuGIT@users.noreply.github.com>
2021-07-19 13:52:05 +0100

    Imported Product Script of sAnagram

--- main.py
+++ main.py
@@ -1,13 +1,36 @@
+import os
 words = ["Xyz","Abc","Aba","Test"]
 
-def solve():
-  x = input("\n  Input Anagram:\n    >>> ")
-  for i in range(len(words)):
+def init():
+    os.system("cls")
+    mode = str(input("\n  Do you want your anagram to be capital sensitive? (Y/N)\n    >>> "))
+    if mode.lower() == "y":
+        solve(mode)
+    elif mode.lower() == "n":
+        solve(mode)
+    else:
+        init()
+
+def solve(mode):
+    if mode == "y":
+        x = input("\n  Input Anagram:\n    >>> ")
+        for i in range(len(words)):
             if sorted(words[i]) == sorted(x):
                 print("\n ",x,"is an anagram of",words[i])
                 input("  Press Enter To Continue")
                 init()
         print("\n ",x,"is not an anagram of any registered word.")
         input("  Press Enter To Continue")
-       
-solve()
+        init()
+    else:
+        x = input("\n  Input Anagram:\n    >>> ")
+        for i in range(len(words)):
+            if sorted(words[i].lower()) == sorted(x.lower()):
+                print("\n ",x,"is an anagram of",words[i])
+                input("  Press Enter To Continue")
+                init()
+        print("\n ",x,"is not an anagram of any registered word.")
+        input("  Press Enter To Continue")
+        init()
+
+init()
